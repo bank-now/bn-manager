cfde7f4b35c8584c2c8e8f959a0a0667bd96dddb
Just1689 <Just1689@gmail.com>
2018-10-03 00:03:20 +0200

    Refactor to use common io

--- main.go
+++ main.go
@@ -1,33 +1,19 @@
 package main
 
 import (
-	"flag"
-	"fmt"
-	"github.com/nsqio/go-nsq"
+	"github.com/bank-now/bn-common-io/queues/pub"
 	"log"
 )
 
-var (
-	topic      = "interest-calculation-v1"
-	nsqAddress = "192.168.88.24:4150"
-	name       = "manager"
-	version    = "v1"
-)
-
 func main() {
-	cfg := nsq.NewConfig()
-	flag.Var(&nsq.ConfigFlag{cfg}, "producer-opt", "http://godoc.org/github.com/nsqio/go-nsq#Config")
-	flag.Parse()
-	cfg.UserAgent = fmt.Sprintf("%s-%s", name, version)
+	c := pub.Config{Address: "192.168.88.24:4150",
+		Version: "v1",
+		Name:    "manager",
+		Topic:   "interest-calculation-v1"}
 
-	producer, err := nsq.NewProducer(nsqAddress, cfg)
+	producer, err := pub.Setup(c)
 	if err != nil {
-		log.Fatalf("failed to create nsq.Producer - %s", err)
-	}
-	for i := 1; i <= 10; i++ {
-		s := fmt.Sprint(i)
-		err = producer.Publish(topic, []byte(s))
-
+		log.Fatal(err)
 	}
 	producer.Stop()
 
