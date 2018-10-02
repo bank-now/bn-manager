aebac8178a693c424138c9c89a89d4f482ae5335
Just1689 <Just1689@gmail.com>
2018-10-02 22:02:06 +0200

    Clean up

--- main.go
+++ main.go
@@ -15,33 +15,18 @@ var (
 func main() {
 	cfg := nsq.NewConfig()
 	flag.Var(&nsq.ConfigFlag{cfg}, "producer-opt", "option to passthrough to nsq.Producer (may be given multiple times, http://godoc.org/github.com/nsqio/go-nsq#Config)")
-
 	flag.Parse()
-
-	//termChan := make(chan os.Signal, 1)
-	//signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM)
-
 	cfg.UserAgent = fmt.Sprintf("manager-v1")
 
-	// make the producers
-	producers := make(map[string]*nsq.Producer)
 	producer, err := nsq.NewProducer(nsqAddress, cfg)
 	if err != nil {
 		log.Fatalf("failed to create nsq.Producer - %s", err)
 	}
-	producers[nsqAddress] = producer
-
 	for i := 1; i <= 10; i++ {
 		s := fmt.Sprint(i)
 		err = producer.Publish(topic, []byte(s))
 
 	}
+	producer.Stop()
 
-	//select {
-	//case <-termChan:
-	//}
-
-	for _, producer := range producers {
-		producer.Stop()
-	}
 }
