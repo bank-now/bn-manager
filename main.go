d3284305fc271a7b37901c77da510a35ddf5a8bd
Just1689 <Just1689@gmail.com>
2018-10-07 23:51:02 +0200

    Zipkin

--- main.go
+++ main.go
@@ -4,7 +4,16 @@ import (
 	"fmt"
 	"github.com/bank-now/bn-common-io/queues/pub"
 	"github.com/bank-now/bn-common-model/common/operation"
+	"github.com/gorilla/mux"
 	"log"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"time"
+
+	"github.com/openzipkin/zipkin-go"
+	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
+	logreporter "github.com/openzipkin/zipkin-go/reporter/log"
 )
 
 const (
@@ -31,4 +40,85 @@ func main() {
 	}
 	producer.Stop()
 
+}
+
+func Example() {
+	// set up a span reporter
+	reporter := logreporter.NewReporter(log.New(os.Stderr, "", log.LstdFlags))
+	defer reporter.Close()
+
+	// create our remote service endpoint
+	endpoint, err := zipkin.NewEndpoint("myService", "192.168.88.24:9411")
+	if err != nil {
+		log.Fatalf("unable to create local endpoint: %+v\n", err)
+	}
+
+	// initialize our tracer
+	tracer, err := zipkin.NewTracer(reporter, zipkin.WithLocalEndpoint(endpoint))
+	if err != nil {
+		log.Fatalf("unable to create tracer: %+v\n", err)
+	}
+
+	// create global zipkin http server middleware
+	serverMiddleware := zipkinhttp.NewServerMiddleware(
+		tracer, zipkinhttp.TagResponseSize(true),
+	)
+
+	// create global zipkin traced http client
+	client, err := zipkinhttp.NewClient(tracer, zipkinhttp.ClientTrace(true))
+	if err != nil {
+		log.Fatalf("unable to create client: %+v\n", err)
+	}
+
+	// initialize router
+	router := mux.NewRouter()
+
+	// start web service with zipkin http server middleware
+	ts := httptest.NewServer(serverMiddleware(router))
+	defer ts.Close()
+
+	// set-up handlers
+	router.Methods("GET").Path("/some_function").HandlerFunc(someFunc(client, ts.URL))
+	router.Methods("POST").Path("/other_function").HandlerFunc(otherFunc(client))
+
+}
+
+func someFunc(client *zipkinhttp.Client, url string) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("some_function called with method: %s\n", r.Method)
+
+		// retrieve span from context (created by server middleware)
+		span := zipkin.SpanFromContext(r.Context())
+		span.Tag("custom_key", "some value")
+
+		// doing some expensive calculations....
+		time.Sleep(25 * time.Millisecond)
+		span.Annotate(time.Now(), "expensive_calc_done")
+
+		newRequest, err := http.NewRequest("POST", url+"/other_function", nil)
+		if err != nil {
+			log.Printf("unable to create client: %+v\n", err)
+			http.Error(w, err.Error(), 500)
+			return
+		}
+
+		ctx := zipkin.NewContext(newRequest.Context(), span)
+
+		newRequest = newRequest.WithContext(ctx)
+
+		res, err := client.DoWithAppSpan(newRequest, "other_function")
+		if err != nil {
+			log.Printf("call to other_function returned error: %+v\n", err)
+			http.Error(w, err.Error(), 500)
+			return
+		}
+		res.Body.Close()
+	}
+}
+
+func otherFunc(client *zipkinhttp.Client) http.HandlerFunc {
+	return func(w http.ResponseWriter, r *http.Request) {
+		log.Printf("other_function called with method: %s\n", r.Method)
+		time.Sleep(50 * time.Millisecond)
+	}
 }
