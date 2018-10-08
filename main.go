3b8309fdd833f6d8e8c6c073ed5b30b6e4951e6c
Just1689 <Just1689@gmail.com>
2018-10-08 23:20:21 +0200

    Clean up

--- main.go
+++ main.go
@@ -3,30 +3,30 @@ package main
 import (
 	"fmt"
 	"github.com/bank-now/bn-common-io/queues/pub"
+	"github.com/bank-now/bn-common-io/zipkin"
 	"github.com/bank-now/bn-common-model/common/operation"
-	"github.com/gorilla/mux"
+	"github.com/nsqio/go-nsq"
 	"log"
-	"net/http"
-	"net/http/httptest"
-	"os"
 	"time"
-
-	"github.com/openzipkin/zipkin-go"
-	zipkinhttp "github.com/openzipkin/zipkin-go/middleware/http"
-	logreporter "github.com/openzipkin/zipkin-go/reporter/log"
 )
 
 const (
-	version = "v1"
-	name    = "manager"
-	Address = "192.168.88.24:4150"
+	version   = "v1"
+	name      = "manager"
+	Address   = "192.168.88.24:4150"
+	ZipKinUrl = "http://192.168.88.24:9411/api/v2/spans"
+)
+
+var (
+	FullName = fmt.Sprint(name, "-", version)
+	action   = fmt.Sprint(operation.InterestOperationV2Topic, ".", "createItem")
 )
 
 func main() {
 	c := pub.Config{Address: Address,
 		Name:    name,
 		Version: version,
-		Topic:   operation.InterestOperationV1Topic}
+		Topic:   operation.InterestOperationV2Topic}
 
 	producer, err := pub.Setup(c)
 	if err != nil {
@@ -34,7 +34,9 @@ func main() {
 	}
 
 	for i := 1; i <= 10; i++ {
-		item := operation.NewInterestOperationV1(fmt.Sprint(i))
+		ghost := addOneWorkItem(producer, c, fmt.Sprint(i))
+		item := operation.NewInterestOperationV2(fmt.Sprint(i))
+		item.Ghost = ghost
 		b, _ := item.ToJsonBytes()
 		producer.Publish(c.Topic, b)
 	}
@@ -42,83 +44,14 @@ func main() {
 
 }
 
-func Example() {
-	// set up a span reporter
-	reporter := logreporter.NewReporter(log.New(os.Stderr, "", log.LstdFlags))
-	defer reporter.Close()
+func addOneWorkItem(producer *nsq.Producer, c pub.Config, acc string) zipkin.Ghost {
+	start := time.Now()
+	item := operation.NewInterestOperationV2(fmt.Sprint(acc))
+	b, _ := item.ToJsonBytes()
+	producer.Publish(c.Topic, b)
 
-	// create our remote service endpoint
-	endpoint, err := zipkin.NewEndpoint("myService", "192.168.88.24:9411")
-	if err != nil {
-		log.Fatalf("unable to create local endpoint: %+v\n", err)
-	}
+	ns := time.Since(start).Nanoseconds()
+	ghost := zipkin.LogParent(ZipKinUrl, FullName, action, ns)
+	return ghost
 
-	// initialize our tracer
-	tracer, err := zipkin.NewTracer(reporter, zipkin.WithLocalEndpoint(endpoint))
-	if err != nil {
-		log.Fatalf("unable to create tracer: %+v\n", err)
-	}
-
-	// create global zipkin http server middleware
-	serverMiddleware := zipkinhttp.NewServerMiddleware(
-		tracer, zipkinhttp.TagResponseSize(true),
-	)
-
-	// create global zipkin traced http client
-	client, err := zipkinhttp.NewClient(tracer, zipkinhttp.ClientTrace(true))
-	if err != nil {
-		log.Fatalf("unable to create client: %+v\n", err)
-	}
-
-	// initialize router
-	router := mux.NewRouter()
-
-	// start web service with zipkin http server middleware
-	ts := httptest.NewServer(serverMiddleware(router))
-	defer ts.Close()
-
-	// set-up handlers
-	router.Methods("GET").Path("/some_function").HandlerFunc(someFunc(client, ts.URL))
-	router.Methods("POST").Path("/other_function").HandlerFunc(otherFunc(client))
-
-}
-
-func someFunc(client *zipkinhttp.Client, url string) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("some_function called with method: %s\n", r.Method)
-
-		// retrieve span from context (created by server middleware)
-		span := zipkin.SpanFromContext(r.Context())
-		span.Tag("custom_key", "some value")
-
-		// doing some expensive calculations....
-		time.Sleep(25 * time.Millisecond)
-		span.Annotate(time.Now(), "expensive_calc_done")
-
-		newRequest, err := http.NewRequest("POST", url+"/other_function", nil)
-		if err != nil {
-			log.Printf("unable to create client: %+v\n", err)
-			http.Error(w, err.Error(), 500)
-			return
-		}
-
-		ctx := zipkin.NewContext(newRequest.Context(), span)
-
-		newRequest = newRequest.WithContext(ctx)
-
-		res, err := client.DoWithAppSpan(newRequest, "other_function")
-		if err != nil {
-			log.Printf("call to other_function returned error: %+v\n", err)
-			http.Error(w, err.Error(), 500)
-			return
-		}
-		res.Body.Close()
-	}
-}
-
-func otherFunc(client *zipkinhttp.Client) http.HandlerFunc {
-	return func(w http.ResponseWriter, r *http.Request) {
-		log.Printf("other_function called with method: %s\n", r.Method)
-		time.Sleep(50 * time.Millisecond)
-	}
 }
